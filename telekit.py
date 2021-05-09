e38c8be8bbd2f9d9f7ce249ca4bf958d490c2d8a
dagimal <daffagifariakmal@gmail.com>
2021-05-10 06:20:56 +0700

    telethon standalone

---
+++ telekit.py
@@ -0,0 +1,14 @@
+"""
+Telegram Adder Group v0.2
+==========================
+Author   : Dagimal
+E-mail   : daffagifariakmal@gmail.com
+Date     : 26-April-2021
+Platform : POSIX, Windows
+--------------------------
+Project ini adalah fork dari original author https://python.gotrained.com/author/majid-alizadeh/
+dengan menambah beberapa fitur di dalamnya
+"""
+
+import os
+
